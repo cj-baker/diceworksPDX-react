0b768dfae1378bd196a3831c85f33dfaf5d50075
Casey Baker <cj-baker@live.com>
2024-06-10 13:25:30 -0700

    React file structure created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      export default Navbar
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>DiceworksPDX</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
