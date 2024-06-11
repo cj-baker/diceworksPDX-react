54958c5c939e6748784fc81c9c4ea34a5ee56e2a
Casey Baker <cj-baker@live.com>
2024-06-11 14:45:32 -0700

    Completed homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navigation from "./Navigation"
+import Hexagons from './Hexagons';
+import Carousel from "./Carousel"
 
 function App() {
   return (
     <div className="App">
-      export default Navbar
+      <Navigation />
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
         <h1>DiceworksPDX</h1>
-        
+        <Hexagons />
+        <h2 class="my-3">Custom dice to fit your playstyle.</h2>
+        <a href="#homepage" id="header-button-link" title="Homepage"
+          ><button
+            type="button"
+            class="btn btn-outline-light"
+            id="header-button"
+          >
+            Let's Roll
+          </button></a>
       </header>
+      <main className='App-main Container'>
+      <h2 id="homepage">Recent Works</h2>
+      <Carousel />
+      </main>
     </div>
   );
 }
