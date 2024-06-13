9f72bfe0a75662a99bfd1cc435ba96baacd605ac
Casey Baker <cj-baker@live.com>
2024-06-12 19:13:29 -0700

    fixed home icon link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ export default function Navigation() {
     return (
         <Navbar expand="lg" bg="dark" fixed="top">
           <Container fluid>
-            <Navbar.Brand href="/"><FaDiceD20 color="#f1e383" size="1.8rem"/></Navbar.Brand>
+            <Navbar.Brand href="#/"><FaDiceD20 color="#f1e383" size="1.8rem"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
               <Nav className="justify-content-end"  variant="underline"activeKey="/home">
