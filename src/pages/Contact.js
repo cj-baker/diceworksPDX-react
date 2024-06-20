ff4cf458d4b6663219be138f3a5a237fea46c210
Casey Baker <cj-baker@live.com>
2024-06-19 17:32:22 -0700

    Added Contact page content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,10 +12,13 @@ return(
         <h2>Contact Us</h2>
         </header>
         <main className="contact-main">
-        <img src="" alt="" />
-            You can reach me on the Noha's Art Photography facebook page, or by email at nohasart06@gmail.com.
-        <br/>
-                Ready to roll some new dice for your next adventure? Download and fill out a <a className="order-link" href="../files/">Custom Dice Order Sheet</a> and email it over and we can get pouring!
+            <p>You can shoot us a quick DM on the DiceworksPDX Instagram, or email us at diceworkspdx@gmail.com.
+            <br/>
+            Ready to roll some new dice for your next adventure? Download and fill out a <a className="order-link" href="../files/">Custom Dice Order Sheet</a> and email it over and we can get pouring!
+            </p>
+        <img className="contact-image" src="https://lh3.googleusercontent.com/d/19e31wHaf2uldIi8Fw8s438Hz8WRtNGM7" title="DiceworksPDX CEO of Cute" alt="cat laying on bed"/>
+        **Hover for important info**
+        
         </main>
         <Footer/>
         </body>
